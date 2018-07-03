30d90c085c01ad0c25b366f826ad99cc618a7222
zhengweihao <wujia.zwh@raycloud.com>
2018-07-03 17:12:19 +0800

    增加检查次日天气功能；

--- src/main/java/com/zeiganga/gate/schedule/NotifySchedule.java
+++ src/main/java/com/zeiganga/gate/schedule/NotifySchedule.java
@@ -1,8 +1,11 @@
 package com.zeiganga.gate.schedule;
 
-import com.zeiganga.gate.thirdparty.dingtalk.MachineMessageSender;
+import com.zeiganga.gate.thirdparty.dingtalk.DingtalkMessageSender;
+import com.zeiganga.gate.thirdparty.weather.Weather;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
 
 /**
  * 消息通知定时任务
@@ -13,22 +16,45 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotifySchedule {
 
-    private static Boolean notified = false;
+    private static final int RETRY_MAX_TIME = 10;// 查询失败的最大重试次数
+    private static int RETRY_TIME = 0;// 当前重试次数
+    private static final long RETRY_INTERVAL = 60000;// 重试间隔时间
 
     /**
      * 测试通知
      */
-    @Scheduled(cron = "0 0 3 * * ?")
-    public void testNotify() {
-        if (notified) {// 不重复通知
+    @Scheduled(cron = "0 0 20 * * ?")
+    public void weatherNotify() {
+        checkRanyWeather(false);
+    }
+
+    /**
+     * 检查次日天气是否为雨天
+     */
+    public void checkRanyWeather(boolean retry) {
+        if (!retry) {
+            RETRY_TIME = 0;
+        } else if (++RETRY_TIME <= RETRY_MAX_TIME) {// 超过最大重试次数
             return;
         }
 
-        for (int i = 0; i < 10; i++) {
-            String content = "起床尿尿了！";
-            boolean atAll = true;
-            MachineMessageSender.sendSimpleMessage(MachineMessageSender.EATING_GROUP_MACHINE_WEBHOOK, content, atAll);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        Weather weather = null;// WeatherHelper.getByCityNameAndDate("杭州", calendar.getTime());
+        if (weather == null) {// 尝试重试
+            try {
+                Thread.sleep(RETRY_INTERVAL);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            checkRanyWeather(true);
+        } else {
+            String type = weather.getType();
+            if (type.contains("雨")) {
+                String content = "明天天气：" + type + "，" + weather.getNotice();
+                DingtalkMessageSender.sendSimpleMessage(DingtalkMessageSender.EATING_GROUP_MACHINE_WEBHOOK, content, true);
+            }
         }
-        notified = true;
     }
+
 }
