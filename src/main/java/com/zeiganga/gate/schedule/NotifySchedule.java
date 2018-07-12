bf5364887440b11d2d73d4f18430939264a645e3
zhengweihao <wujia.zwh@raycloud.com>
2018-07-12 14:45:29 +0800

    修复天气检查执行异常问题；

--- src/main/java/com/zeiganga/gate/schedule/NotifySchedule.java
+++ src/main/java/com/zeiganga/gate/schedule/NotifySchedule.java
@@ -13,9 +13,9 @@ import java.util.Calendar;
 
 /**
  * 消息通知定时任务
- * User: ZhengWeihao
- * Date: 2018-06-30
- * Time: 16:01:08
+ * @Author: ZhengWeihao
+ * @Date: 2018-06-30
+ * @Time: 16:01:08
  */
 @Component
 @EnableScheduling
@@ -23,9 +23,22 @@ public class NotifySchedule {
 
     private static final CustomLogger logger = CustomLogger.getLogger(NotifySchedule.class);
 
-    private static final int RETRY_MAX_TIME = 10;// 查询失败的最大重试次数
-    private static int RETRY_TIME = 0;// 当前重试次数
-    private static final long RETRY_INTERVAL = 60000;// 重试间隔时间
+    /**
+     * 查询失败的最大重试次数
+     */
+    private static final int RETRY_MAX_TIME = 10;
+    /**
+     * 当前重试次数
+     */
+    private static int RETRY_TIME = 0;
+    /**
+     * 重试间隔时间
+     */
+    private static final long RETRY_INTERVAL = 60000;
+    /**
+     * 需要提醒的关键字
+     */
+    private static final String NOTIFY_WORD = "雨";
 
     /**
      * 测试通知
@@ -42,7 +55,8 @@ public class NotifySchedule {
     public static void checkRanyWeather(boolean retry) {
         if (!retry) {
             RETRY_TIME = 0;
-        } else if (++RETRY_TIME <= RETRY_MAX_TIME) {// 超过最大重试次数
+        } else if (++RETRY_TIME >= RETRY_MAX_TIME) {
+            // 超过最大重试次数
             logger.biz("达到最大重试次数，放弃，RETRY_TIME：{}，RETRY_MAX_TIME：{}", RETRY_TIME, RETRY_MAX_TIME);
             return;
         }
@@ -50,7 +64,8 @@ public class NotifySchedule {
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_MONTH, 1);
         Weather weather = WeatherHelper.getByCityNameAndDate("杭州", calendar.getTime());
-        if (weather == null) {// 尝试重试
+        if (weather == null) {
+            // 尝试重试
             try {
                 Thread.sleep(RETRY_INTERVAL);
             } catch (InterruptedException e) {
@@ -60,12 +75,15 @@ public class NotifySchedule {
         } else {
             logger.biz("查询到次日天气，weather：{}", JSON.toJSONString(weather));
             String type = weather.getType();
-            if (type.contains("雨")) {
+            boolean needNotify = type.contains(NOTIFY_WORD);
+            if (needNotify) {
                 String content = "明天天气：" + type + "，" + weather.getFx() + " " + weather.getFl() + "，最低温度"
                         + weather.getLow() + "，" + weather.getNotice();
                 DingtalkMessageSender.sendSimpleMessage(DingtalkMessageSender.EATING_GROUP_MACHINE_WEBHOOK, content, true);
                 logger.biz("完成天气预警通知，content：{}", content);
+                return;
             }
+            logger.biz("天气检查完成，type：{}，needNotify：{}", type, needNotify);
         }
     }
 
