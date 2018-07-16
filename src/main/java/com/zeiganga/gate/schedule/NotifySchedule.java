6116953cce931bf0a68a1022067a9bb2eac925f2
zhengweihao <wujia.zwh@raycloud.com>
2018-07-16 19:02:16 +0800

    增加每月提醒；

--- src/main/java/com/zeiganga/gate/schedule/NotifySchedule.java
+++ src/main/java/com/zeiganga/gate/schedule/NotifySchedule.java
@@ -36,11 +36,21 @@ public class NotifySchedule {
     /**
      * 月底提示
      */
-    @Scheduled(cron = "0 0 16 L * ?")
+    @Scheduled(cron = "0 0 16 * * ?")
     public void monthlyNotify() {
+        logger.biz("开始检查月底提醒");
+        Calendar calendar = Calendar.getInstance();
+        boolean isLastDayOfMonth = calendar.get(Calendar.DAY_OF_MONTH) == calendar
+                .getActualMaximum(Calendar.DAY_OF_MONTH);
+        if (!isLastDayOfMonth) {
+            logger.biz("不是当月最后一天，跳过此任务");
+            return;
+        }
+
         String content = "一个月又过去咯，这个月的目标都实现了吗，下个月的目标又是什么呢？";
         boolean atAll = true;
         DingtalkMessageSender.sendSimpleMessage(DingtalkMessageSender.EATING_GROUP_MACHINE_WEBHOOK, content, atAll);
+        logger.biz("完成月底提醒");
     }
 
 }
